dbe365df288735da11a048ac22817b9b76748009
David <138037766+dhx101@users.noreply.github.com>
2024-02-13 10:44:56 +0100

    added few links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-
-import './App.css';
-import Home from './components/Home';
-import Scan from './components/Scan';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Scan from "./components/scans/Scan";
+import ScanQR from "./components/scans/ScanQR";
+import ScanBarCode from "./components/scans/ScanBarCode";
 
 function App() {
-  return (
-    <div>
-    <Home/>
-
-      {/* <Scan/> */}
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/scan" element={<Scan/>}/>
+          <Route path="/scanQR" element={<ScanQR/>}/>
+				<Route path="/scanBar" element={<ScanBarCode/>}/>
+        </Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,71 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Home = () => {
-  const showInfo =() => {
-    
-  }
+	const showInfo = () => {};
 	return (
 		<>
 			<header>
-        <nav></nav>
-        <button onClick={showInfo}>i</button>
-      </header>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/profile">Perfil</Link>
+						</li>
+						<li>
+							<Link to="/favorites">Favoritos</Link>
+						</li>
+						<li>
+							<Link to="/diary">Diario</Link>
+						</li>
+						<li>
+							<Link to="/share">Compartir</Link>
+						</li>
+						<li>
+							<Link to="/traductor">Tradcutor</Link>
+						</li>
+						<li>
+							<Link to="/legal">Términos</Link>
+						</li>
+						<li>
+							<Link to="/signout">Salir</Link>
+						</li>
+					</ul>
+				</nav>
+				<button onClick={showInfo}>i</button>
+			</header>
 			<img src="" alt="" />
 			<h1>Applergic</h1>
 			<p>Mi guía alimentaria</p>
 			<ul>
 				<li>
-					<button>Escanear</button>
+					<Link to="/scanBar">Escanear</Link>
 					<p>Escanea un nuevo producto</p>
 				</li>
 				<li>
-					<button>Buscar</button>
+					<Link to="/">Buscar</Link>
 					<p>Busca un comercio o restuarante para ti.</p>
 				</li>
 				<li>
-					<button>SOS</button>
+					<a href="tel:">SOS </a>
 					<p>¿Necesitas ayuda urgente? contactamo con emergencias</p>
 				</li>
 			</ul>
-      <nav>
-
-      </nav>
+			<nav>
+				<ul>
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="/favorites">Favoritos</NavLink>
+					</li>
+					<li>
+						<NavLink to="/diary">Diario</NavLink>
+					</li>
+					<li>
+						<NavLink to="/share">Compartir</NavLink>
+					</li>
+				</ul>
+			</nav>
 		</>
 	);
 };
