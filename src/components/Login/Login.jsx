c121b5e4e0955172a25539c1e4beb5c6a232e630
Thr4sh3r01 <thr4sh3r01@gmail.com>
2024-02-16 18:06:54 +0100

    prueba login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,35 +1,63 @@
-import React from 'react'
-import './Login.css'
-import Image from './Sin título-1.png'
+import React from "react";
+import "./Login.css";
+import Image from "./Sin título-1.png";
+import { JwtContext } from "../../shared/contexts/JwtContext";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
-const Login = () => {
+export default function LoginPage() {
+  const { register, handleSubmit } = useForm();
+  const { setJwt } = useContext(JwtContext);
+
+  const onSubmit = (FormData) => {
+    axios.post("login", formData).then((res) => {
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+    });
+  };
   return (
     <>
-    <img src={Image} alt="imagen cabecera del login"></img>
-    <div className='container'>
-      <div className="header">
-        <div className="text">¡Bienvenido de nuevo!</div>
-        <p>Por Favor, introduce tus datos para continuar.</p>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-         <div className="input">
-          <input type="email" placeholder="Dirección Email"/>
+      <img src={Image} alt="imagen cabecera del login"></img>
+      <div className="container">
+        <div className="header">
+          <div className="text">¡Bienvenido de nuevo!</div>
+          <p>Por Favor, introduce tus datos para continuar.</p>
+          <div className="underline"></div>
         </div>
-        <br></br>
-         <div className="input">
-          <input type="password" placeholder="Password"/>
+        <div className="inputs">
+          <div className="input">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input type="email" placeholder="Dirección Email" />
+              {...register("email", {
+                required: true,
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              })}
+              />
+            </form>
+          </div>
+          <br></br>
+          <div className="input">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="password"
+                placeholder="Password"
+                defaultValue={"ABCedf123"}
+                {...register("password", {
+                  required: true,
+                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+                })}
+              />
+            </form>
+          </div>
         </div>
-      </div>
-      <div className="Forgot-password">¿Olvidaste la contraseña?</div>
-    </div>
-    <div className="submit-container">
-      <div className="submit">Entrar</div>
-      <p>¿Nuevo en Applergic?</p>
-      <div className="Login">Crea tu cuenta aquí</div>
-    </div>
-    </>
-  )
-}
 
-export default Login
+        <div className="Forgot-password">¿Olvidaste la contraseña?</div>
+      </div>
+      <div className="submit-container">
+        <div className="submit">Entrar</div>
+        <p>¿Nuevo en Applergic?</p>
+        <div className="Login">Crea tu cuenta aquí</div>
+      </div>
+    </>
+  );
+}
