ddc3e97588ca438dcc92a95a135af144ef4fb04f
Thr4sh3r01 <thr4sh3r01@gmail.com>
2024-02-17 02:27:51 +0100

    creado Registros

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { API } from "./../../Api";
+import axios from "axios";
 
-const Register = () => {
+export default function RegisterPage() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (formData) => {
+    API.post("register", formData).then((res) => {
+      console.log("Register user");
+    });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="">
+        <Link to="/">Volver</Link>;{/* aqui iría la numeración de páginas */}
+        <h2>Dinos quien eres.</h2>
+      </div>
+      <div className="">
+        <img src="../../../public/assets/subir-foto.png" />
+      </div>
+    </>
+  );
 }
-
-export default Register
